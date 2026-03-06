cef31f8688e80b715f75d4c8f588e14f3f4b8610
Saicharan Sathyam <36804057+saicharansathyam@users.noreply.github.com>
2026-03-06 17:01:11 +0100

    Update run_pdc_test.sh

--- app/run_pdc_test.sh
+++ app/run_pdc_test.sh
@@ -190,6 +190,7 @@ run_apps() {
     QT_QPA_PLATFORM=wayland \
     WAYLAND_DISPLAY=wayland-1 \
     QT_WAYLAND_DISABLE_WINDOWDECORATION=1 \
+    QT_QUICK_BACKEND=software \
     XDG_RUNTIME_DIR="$XDG_RT" \
     VSOMEIP_APPLICATION_NAME=GearApp \
     VSOMEIP_CONFIGURATION="$SCRIPT_DIR/GearApp/config/vsomeip_ecu2.json" \
@@ -203,6 +204,7 @@ run_apps() {
     QT_QPA_PLATFORM=wayland \
     WAYLAND_DISPLAY=wayland-1 \
     QT_WAYLAND_DISABLE_WINDOWDECORATION=1 \
+    QT_QUICK_BACKEND=software \
     XDG_RUNTIME_DIR="$XDG_RT" \
     VSOMEIP_APPLICATION_NAME=PDCApp \
     VSOMEIP_CONFIGURATION="$SCRIPT_DIR/PDCApp/config/vsomeip_pdc.json" \
